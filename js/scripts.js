a46f7ee409e0d03ad9cbbe218650a73720fcb5b3
William Pratt <willpratt78@gmail.com>
2020-03-25 15:48:08 -0700

    add each type of input types covered

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var console = $("#gaming-platform").val();
+
+  var hobby = $("input:radio[name=activity]:checked").val();
+
+  var dob = $("#born").val();
+
+  var favoriteolor = $("#color").val();
+
+  $("#output").text(result);
+});
